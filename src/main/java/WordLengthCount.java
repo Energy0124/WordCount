5488198377525c464cf627113b6026c739722070
LING <1155062557@link.cuhk.edu.hk>
2017-10-10 13:34:45 +0800

    Initial commit

---
+++ src/main/java/WordLengthCount.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordLengthCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, IntWritable, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private IntWritable count = new IntWritable(0);
+        private IntWritable wordLength = new IntWritable(0);
+        Map<IntWritable, IntWritable> inmemMap;
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                wordLength.set(itr.nextToken().length());
+                if(inmemMap.containsKey(wordLength)){
+                    count.set(inmemMap.get(wordLength).get() + 1);
+                    System.out.println(wordLength.get()+" : "+count.get());
+                    inmemMap.put(new IntWritable(wordLength.get()), new IntWritable(count.get()));
+                }else{
+                    inmemMap.put(new IntWritable(wordLength.get()), new IntWritable(one.get()));
+                    System.out.println(wordLength.get()+" : "+1);
+                }
+            }
+        }
+
+        protected void setup(Mapper.Context context) throws IOException, InterruptedException {
+            inmemMap  = new HashMap<IntWritable, IntWritable>();
+        }
+
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            for (Map.Entry<IntWritable, IntWritable> entry : inmemMap.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+                context.write(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<IntWritable,IntWritable,IntWritable,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word length count");
+        job.setJarByClass(WordLengthCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
